7d2ff72a5334c5eca6bd6145ae37965d1f981b48
xiii111 <aoi.t19950517@gmail.com>
2022-04-27 10:44:09 +0900

    Initial commit

---
+++ ParticleInSwiftUI/ParticleInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParticleInSwiftUIApp.swift
+//  ParticleInSwiftUI
+//
+//  Created by 高橋碧 on 2022/04/27.
+//
+
+import SwiftUI
+
+@main
+struct ParticleInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
